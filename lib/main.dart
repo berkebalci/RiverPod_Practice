6f0337a4dedc89104ddf8192d22f3b2d85c09f4c
berkebalci <berkebalcii38@gmail.com>
2022-07-13 16:49:10 +0300

    Riverpod StateManagement eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
+final counterProvider = StateProvider(((ref) {
+  return 0;
+})); //Read ve Modify yapabilcez
+//Buradaki 0 counterProvider'in state'sidir.
+
+//'autoDispose' ile widget'in dispose olduğu durumlarda(Farklı bir sayfaya geçme vs.)
+//**bu widget'e bağlı Provider'in da dispose olmasını sağlıyoruz.
+
+//Böylece yaptığımız uygulamada farklı bir sayfaya geçtiğimizde counterProvider oto
+//**silindiğinden dolayı 0'dan tekrar tekrar başlıyor.
+
+Future main() async {
+  //Firebase kullanacağımızdan dolayı Future ifadesi var.
+  WidgetsFlutterBinding.ensureInitialized(); //Firebase bağlantısı
+  await Firebase.initializeApp();
+
+  runApp(ProviderScope(
+      //Riverpod ile Flutter'ı bağladığımız yer!!
+      child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,106 +29,103 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //Burdaki 'ref' ile watch,listen ve read yapabileceğiz.
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        backgroundColor: Colors.white,
         primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.green,
+            brightness: Brightness.dark,
+            surface: Color(0Xff003909)),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Home"),
+        ),
+        body: Center(
+            child: ElevatedButton(
+          child: Text("Go to the Counter page"),
+          onPressed: () {
+            Navigator.of(context).push(
+              //Navigation
+              MaterialPageRoute(builder: (((context) => CounterPage()))),
+            );
+          },
+        )));
+  }
+}
+
+class CounterPage extends ConsumerWidget {
+  //Widget'in Providerlara erişmesi için  elzem nokta!!
+  const CounterPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int counter = ref.watch(counterProvider); //Böylece counterProvider'in
+    //**state'i her değiştiğinde bu widget rebuild olacak.
+    //Bu aslında Getx'de olan variable.obs yapısına benzemektedir.
+
+    //watch state'de değişiklik gördüğünde içinde olduğu widget'i rebuild etmektedir.
+
+    //**Sayı 5'i geçtiği zaman uyarı barı göstermek(Alert bar) */
+    ref.listen<int>(counterProvider, ((previous, next) {
+      //Burdaki int ifadesini eklemessek 'if' ifadesinde hata alırız.
+      //next = current value
+      if (next > 6) {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text("Alert!"),
+                content:
+                    Text("Number is dangerously high.Please decrease it!."),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Alright!"))
+                ],
+              );
+            });
+      }
+    }));
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Counter Page"),
+        leading: IconButton(
+            //Ekrandaki sayinin default değere(state'e) dönmesini sağlıcak.
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              ref.invalidate(
+                  counterProvider); //Böylece counterProvider'e default state'ini verdik.
+            }),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+          child: Text(
+        '$counter',
+        style: Theme.of(context).textTheme.displayMedium,
+      )),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.add),
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++; //state'i arttiriyoruz.
+          //State bizim durumumuzda
+        },
+      ),
     );
   }
 }
